13d04bf283e6545565007254c8174fdb165233bd
María Naranjo Almeida <maria2000naranjo@gmail.com>
2021-03-17 12:11:08 +0000

    Added Simple Events

--- src/index.js
+++ src/index.js
@@ -41,13 +41,26 @@ function BookList() {
 
 //Props Destructuring
 const Book = ({ img, title, author } /*props*/) => {
-  //const { img, title, author } = props;
+
+  //attribute, eventHandler
+  //onClick, onMouseOver
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+  }
+  const complexExample = (author) => {
+    console.log(author);
+  }
   return (
-    <article className='book'>
+    <article className='book' onMouseOver={() => {
+      console.log(title);
+    }}>
       <img
         src={img} alt="book" />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>Example</button>
+      <button type='button' onClick={() => complexExample(author)/*If we don't add the () => it will run when we open de app  */}>More complex example</button>
     </article>
   );
 }
@@ -60,6 +73,7 @@ ReactDom.render(<BookList />, document.getElementById('root'));
  * Important things:
  * -You always have to close tags
  * -The name of a component must be in capital letter
+ * -Events in React are written in camelCase
  */
 
 /**
